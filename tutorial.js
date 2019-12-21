cb1daceeaa68ff3eb0823cb31d2bcd43cf54a59e
Steve McKinnon <mckinn_git@yahoo.com>
2019-12-21 12:14:40 -0500

    Initial Commit with Slack SDK

---
+++ tutorial.js
@@ -0,0 +1,25 @@
+const { WebClient } = require('@slack/web-api');
+
+console.log('Getting started with Node Slack SDK');
+
+// Create a new instance of the WebClient class with the token read from your environment variable
+const web = new WebClient(process.env.SLACK_TOKEN);
+// The current date
+const currentTime = new Date().toTimeString();
+
+(async () => {
+  // Use the `auth.test` method to find information about the installing user
+  const res = await web.auth.test()
+
+  // Find your user id to know where to send messages to
+  const userId = res.user_id
+  console.log(userId);
+
+  // Use the `chat.postMessage` method to send a message from this app
+  await web.chat.postMessage({
+    channel: userId,
+    text: `The current time is ${currentTime}`,
+  });
+
+  console.log('Message posted!');
+})();
