f3e74b78e3a39becc5e31beb4bbb0dbf1950f5cd
Steve McKinnon <mckinn_git@yahoo.com>
2019-12-22 22:02:56 -0500

    Update expressTrial.js
    Expect and respond to a challenge

--- expressTrial.js
+++ expressTrial.js
@@ -7,6 +7,7 @@ const express = require('express'),
  //note - this port is meaningless to Heroku, 
  // but that does not seem to matter
 const localPort = process.env.PORT || 9000;
+console.log("AUth token is: " + process.env.SLACK_TOKEN);
 const web = new WebClient(process.env.SLACK_TOKEN); 
 const thing = {
     name: 'Steve',
@@ -36,22 +37,50 @@ server.use(bodyParser.json());  // parse JSON bodies
 
 server.set('port', localPort);
 
-//Adding routes
-server.get('/',(request,response)=>{
- response.send('Hello World');
+process.on('SIGINT', () => {
+  console.log('Control C received.');
+  process.exit();
 });
 
-server.get('/thing',(request,response)=>{
+process.on('beforeExit', (code) => {
+  console.log('Process beforeExit event with code: ', code);
+});
+
+process.on('exit', (code) => {
+  console.log('Process exit event with code: ', code);
+});
+
+//Adding routes
+
+server.get('/',(request,response)=>{
+  console.log('Hello World');
+  response.json({text:"Hello World"});
+});
+
+server.get('/slack',(request,response)=>{
+  console.log('validation challenge');
+  const body = request.body;
+  console.log(JSON.stringify(body));
+  // const cha = (({challenge}) => ({challenge}))(body); // destructure the challenge property to an object
+  const cha = (                 // IFFE
+    function({challenge}) {     // assign challenge the value from the passed object
+      return {challenge};       // return an object, rather than the thing itself
+    }
+  )(body);                      // execute the thing
+  console.log(JSON.stringify(cha));
+  response.json(cha);
+});
+
+server.get('/client/thing',(request,response)=>{
  response.json(thing);
 });
 
-server.put('/echo',(request,response)=>{
+server.put('/client/echo',(request,response)=>{
     response.json(request.body);
     let {textToSend = "Oh no - nothing"} = request.body;
     pingUser(textToSend);
    });
 
 server.listen(server.get('port'),()=>{
-    // console.log(`Express server started at port ${localPort}`);
     console.log('Express server started at port: ', server.get('port') );
 });
