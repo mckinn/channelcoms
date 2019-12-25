7a960ef7d5b7f04464804bf790635c8a95cf2a6e
Steve McKinnon <mckinn_git@yahoo.com>
2019-12-25 13:18:21 -0500

    Second attempt at auth exchange
    updated the view description and the first (intentionally broken) auth attempt stage.

--- expressTrial.js
+++ expressTrial.js
@@ -1,86 +1,165 @@
 //server.js
 const express = require('express'),
-      server = express(),
-      bodyParser = require('body-parser'),
-      { WebClient } = require('@slack/web-api');
+    server = express(),
+    bodyParser = require('body-parser'),
+    {
+        WebClient
+    } = require('@slack/web-api'),
+    mongoose = require('mongoose');
 
- //note - this port is meaningless to Heroku, 
- // but that does not seem to matter
+
+//note - this port is meaningless to Heroku, 
+// but that does not seem to matter
 const localPort = process.env.PORT || 9000;
-console.log("AUth token is: " + process.env.SLACK_TOKEN);
-const web = new WebClient(process.env.SLACK_TOKEN); 
-const thing = {
-    name: 'Steve',
-    location: 'Canada',
-}
+const databaseUrl = process.env.MONDATABASE;
+const appToken = process.env.SLACK_TOKEN;
+console.log("Database URL is: " + databaseUrl);
 
-async function pingUser (textToSend = "hi there") {
+mongoose.connect(databaseUrl, {
+    useNewUrlParser: true
+});
+const db = mongoose.connection;
+
+console.log("Auth token is: " + appToken);
+const web = new WebClient(appToken);
+
+async function pingUser(textToSend = "hi there") {
     // Use the `auth.test` method to find information about the installing user
     const res = await web.auth.test()
-  
+
     // Find your user id to know where to send messages to
     const userId = res.user_id
     console.log(userId);
     console.log(JSON.stringify(res));
-  
+
     // Use the `chat.postMessage` method to send a message from this app
     await web.chat.postMessage({
-      channel:  "C9XG2PBTN",   // General for now
-      text: textToSend,
+        channel: "C9XG2PBTN", // General for now
+        text: textToSend,
     });
-      console.log(`Message ${textToSend} posted!`);
-  }
+    console.log(`Message ${textToSend} posted!`);
+}
 
-console.log('thing: '+JSON.stringify(thing));
+db.on('error', () => {
+    console.error.bind(console, 'connection error:');
+    // process.exit(1)
+});
 
-server.use(bodyParser.json());  // parse JSON bodies
+let credsSchema = {},
+    credModel = {};
+
+// TODO change the flow here to ensure that all of the initialization is done
+// in one Promises chain to avoid issues with race conditions.
+
+db.once('open', function () {
+    // we're connected!
+    console.log(`Database ${databaseUrl} connected`);
+    // create schema (class) and model
+    credsSchema = new mongoose.Schema({
+        access_token: String,
+        scope: String,
+        team_name: String,
+        team_id: String,
+        bot: {
+            bot_user_id: String,
+            bot_access_token: String,
+        }
+    });
+    // as schema change the model needs to be re-generated
+    credModel = mongoose.model('UserCredentials', credsSchema);
+
+});
+
+server.use(bodyParser.json()); // parse JSON bodies
 
 server.set('port', localPort);
 
 process.on('SIGINT', () => {
-  console.log('Control C received.');
-  process.exit();
+    console.log('Control C received.');
+    process.exit();
 });
 
 process.on('beforeExit', (code) => {
-  console.log('Process beforeExit event with code: ', code);
+    console.log('Process beforeExit event with code: ', code);
 });
 
 process.on('exit', (code) => {
-  console.log('Process exit event with code: ', code);
+    console.log('Process exit event with code: ', code);
 });
 
 //Adding routes
 
-server.get('/',(request,response)=>{
-  console.log('Hello World');
-  response.json({text:"Hello World"});
+server.get('/', (request, response) => {
+    console.log('Hello World');
+    response.json({
+        text: "Hello World"
+    });
 });
 
-server.post('/slack',(request,response)=>{
-  console.log('validation challenge');
-  const body = request.body;
-  console.log(JSON.stringify(body));
-  // const cha = (({challenge}) => ({challenge}))(body); // destructure the challenge property to an object
-  const cha = (                 // IFFE
-    function({challenge}) {     // assign challenge the value from the passed object
-      return {challenge};       // return an object, rather than the thing itself
-    }
-  )(body);                      // execute the thing
-  console.log(JSON.stringify(cha));
-  response.json(cha);
+server.post('/slack', (request, response) => {
+    console.log('validation challenge');
+    const body = request.body;
+    console.log(JSON.stringify(body));
+    // const cha = (({challenge}) => ({challenge}))(body); // destructure the challenge property to an object
+    const cha = ( // IFFE
+        function ({
+            challenge
+        }) { // assign challenge the value from the passed object
+            return {
+                challenge
+            }; // return an object, rather than the thing itself
+        }
+    )(body); // execute the thing
+    console.log(JSON.stringify(cha));
+    response.json(cha);
 });
 
-server.get('/client/thing',(request,response)=>{
- response.json(thing);
+server.post('/slack/authAttempt', (request, response) => {
+    console.log('authorization attempt');
+    const body = request.body;
+    console.log(JSON.stringify(body));
+    /*
+    const temCreds = (({code}) => ({code}))(body); // destructure the challenge property to an object
+    const creds = ( // IFFE
+        function ({
+            access_token,
+            scope,
+            team_name,
+            team_id,
+            bot
+        }) { // assign challenge the value from the passed object
+            return {
+                access_token,
+                scope,
+                team_name,
+                team_id,
+                bot
+            }; // return an object, rather than the thing itself
+        }
+    )(body); // execute the thing
+    console.log(JSON.stringify(temCreds));
+     */
+    response.json({
+        code: "029384", // body.code
+        state: "osdjfosdajf", //body.state
+        client_id: "lskdjfldsjf",
+        client_secret: "lsadkfjlsdf"
+    });
+    /*     const cm = new credModel(creds);
+        cm.save(function (err, cm) {
+            if (err) return console.error(err); 
+    });
+    */
 });
 
-server.put('/client/echo',(request,response)=>{
+server.put('/client/echo', (request, response) => {
     response.json(request.body);
-    let {textToSend = "Oh no - nothing"} = request.body;
+    let {
+        textToSend = "Oh no - nothing"
+    } = request.body;
     pingUser(textToSend);
-   });
+});
 
-server.listen(server.get('port'),()=>{
-    console.log('Express server started at port: ', server.get('port') );
+server.listen(server.get('port'), () => {
+    console.log('Express server started at port: ', server.get('port'));
 });
