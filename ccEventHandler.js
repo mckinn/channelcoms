016bb1405a16d740b6d4d3dd74783e7d14eb7075
Steve McKinnon <mckinn_git@yahoo.com>
2019-12-26 23:57:26 -0500

    more data on the view update request

--- ccEventHandler.js
+++ ccEventHandler.js
@@ -33,24 +33,24 @@ const request_options = {
 };
 
 const hao = function (
-    event,
-    token
-) {
-    console.log("*** in home_app_opened handler ***")
-    console.log(JSON.stringify(event));
-    console.log(event.type);
-    hao_view.user_id = event.user;
-    request_options.auth.bearer = token;
-    request_options.body = JSON.stringify(hao_view);
-    const requested = request.post(request_options);
-    console.log(JSON.stringify(requested));
-}
+        event,
+        token
+    ) {
+        console.log("*** in home_app_opened handler ***")
+        console.log(JSON.stringify(event));
+        console.log(event.type);
+        hao_view.user_id = event.user;
+        request_options.auth.bearer = token;
+        request_options.body = JSON.stringify(hao_view);
+        console.log("OPTIONS: " + JSON.stringify(request_options);
+            const requested = request.post(request_options); console.log("REQUESTED: " + JSON.stringify(requested));
+        }
 
-const ccEvents = {
-    app_home_opened: {
-        name: "app_home_opened",
-        handler: hao
-    },
-};
+        const ccEvents = {
+            app_home_opened: {
+                name: "app_home_opened",
+                handler: hao
+            },
+        };
 
-module.exports = ccEvents;
+        module.exports = ccEvents;
