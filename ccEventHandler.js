f5e7f1b7e120fab06636aeaae1a8127553287e38
Steve McKinnon <mckinn_git@yahoo.com>
2019-12-31 19:56:31 -0500

    Update ccEventHandler.js

--- ccEventHandler.js
+++ ccEventHandler.js
@@ -9,7 +9,7 @@ const hao_view = {
             type: 'section',
             text: {
                 type: 'mrkdwn',
-                text: 'Click to Authenticate - APP - 12/27'
+                text: 'Click to Authenticate - APP - 12/31a'
             },
             accessory: {
                 type: 'button',
@@ -17,7 +17,7 @@ const hao_view = {
                     type: 'plain_text',
                     text: 'watch me'
                 },
-                url: 'https://slack.com/oauth/authorize?scope=commands,bot&client_id=337577310114.861150093250&state=arglebargle&redirect_uri=https://channelcoms.herokuapp.com/slack/authAttempt'
+                url: 'https://slack.com/oauth/authorize?scope=channels,groups,im,mpim,sdfsdf,bot&client_id=337577310114.861150093250&state=arglebargle&redirect_uri=https://channelcoms.herokuapp.com/slack/authAttempt'
             }
         }]
     }
